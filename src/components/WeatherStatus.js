13a87b1062e7b8da8e930eb9f3765ad9e0f0c5d2
Adiela Abishua <adiela.abishua@gmail.com>
2024-04-29 22:30:53 +0300

    Edit Weather Status component

--- src/components/WeatherStatus.js
+++ src/components/WeatherStatus.js
@@ -6,7 +6,6 @@ const URL = `https://api.openweathermap.org/data/2.5/weather?q=nairobi&units=met
 
 
 export default function WeatherStatus () {
-    console.log('this is the env', process.env)
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
@@ -23,7 +22,7 @@ export default function WeatherStatus () {
             <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Weather Condition" width={60} height={60} />
             <p className='temperature'>{Math.trunc(data.main.temp)}<sup>°</sup>C</p>
             <p className='weather-condition'>{data.weather[0].main}</p>
-            <p>Humidity: {data.main.humidity}</p>
+
         </div>
     )
 }
