f936c8eb0387ed1324462f862f6e80518fff7135
Adiela Abishua <adiela.abishua@gmail.com>
2024-04-29 17:17:00 +0300

    Add react-intl and add translations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React, { useState } from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import messages_en from './translations/en.json';
+import messages_sw from './translations/sw.json';
 import './App.css';
 import LanguageToggle from "./components/LanguageToggle"
 
+const messages = {
+  en: messages_en,
+  sw: messages_sw,
+};
+
 function App() {
+  const [locale, setLocale] = useState('en'); //Default locale is English
+
+  const changeLanguage = (selectedLocale) => {
+    setLocale(selectedLocale)
+  };
   return (
-    <div id="App">
-      <LanguageToggle/>
-      <h1 id='app-name'>Weather</h1>
-    </div>
+    <IntlProvider messages={messages[locale]}  locale={locale}>
+      <div id="App">
+        <LanguageToggle/>
+        <h1 id='app-name'>
+          <FormattedMessage id="app.title"/>
+        </h1>
+      </div>
+    </IntlProvider>
   );
 }
 
