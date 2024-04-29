55ea2fcf270fc619b46c52ae687c9d858d85076a
Adiela Abishua <adiela.abishua@gmail.com>
2024-04-29 22:36:45 +0300

    Translate weather condition in component

--- src/components/WeatherStatus.js
+++ src/components/WeatherStatus.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './WeatherStatus.css'
+import { FormattedMessage } from 'react-intl';
 
 const URL = `https://api.openweathermap.org/data/2.5/weather?q=nairobi&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_APIKEY}`
 
@@ -21,7 +22,7 @@ export default function WeatherStatus () {
             <h2 className='city-name'>{data.name}</h2>
             <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Weather Condition" width={60} height={60} />
             <p className='temperature'>{Math.trunc(data.main.temp)}<sup>°</sup>C</p>
-            <p className='weather-condition'>{data.weather[0].main}</p>
+            <p className='weather-condition'><FormattedMessage id={`condition.${data.weather[0].main}`.toLowerCase()}/></p>
 
         </div>
     )
